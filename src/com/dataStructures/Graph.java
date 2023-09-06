927e322e67751a4eb16d0b3e43b39ccf66ef07d3
Psarthak <panditsarthak1553@gmail.com>
2023-09-06 17:26:47 +0530

    DP

--- src/com/dataStructures/Graph.java
+++ src/com/dataStructures/Graph.java
@@ -3,17 +3,13 @@ package com.dataStructures;
 import java.util.*;
 
 class Graph {
-
     // BFS Traversal
     static List<Integer> bfs(int start, int n, List<List<Integer>> adj){
         List<Integer> bfs = new ArrayList<>();
         boolean[] visited = new boolean[n]; // n = no of vertices / nodes
         Queue<Integer> q = new LinkedList<>();
-
-        //starting node is 0 but can vary
         q.offer(start);
-        visited[0] = true;
-
+        visited[start] = true;
         while (!q.isEmpty()){
             int node = q.poll();
             bfs.add(node);
@@ -61,6 +57,61 @@ class Graph {
         }
         return dfs;
     }
+    static int islands(char[][] grid){
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        int counter = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if(grid[i][j] == '1' && !visited[i][j]){
+                    counter++;
+                    myBFS(i, j, visited, grid);
+                }
+            }
+        }
+        return counter;
+    }
+    static class XY{
+        int i;
+        int j;
+
+        XY(int i, int j){
+            this.i = i;
+            this.j = j;
+        }
+    }
+    static void myBFS(int x, int y, boolean[][] visited, char[][] grid){
+        Queue<XY> q = new LinkedList<>();
+        q.offer(new XY(x,y));
+        visited[x][y] = true;
+        while (!q.isEmpty()){
+            XY node = q.poll();
+
+            /* Checking the neighbours  */
+            int i = node.i;
+            int j = node.j;
+
+            /* Top */
+            if(i>0 && grid[i-1][j] == '1' && !visited[i-1][j]) {
+                visited[i - 1][j] = true;
+                q.offer(new XY(i - 1, j));
+            }
+            /* Bottom */
+            if(i<grid.length-1 && grid[i+1][j] == '1' && !visited[i+1][j]) {
+                visited[i+1][j] = true;
+                q.offer(new XY(i + 1, j));
+            }
+            /* Left */
+            if(j>0 && grid[i][j-1] == '1' && !visited[i][j-1]) {
+                visited[i][j-1] = true;
+                q.offer(new XY(i, j - 1));
+            }
+            /* Right */
+            if(j< grid[0].length-1 && grid[i][j+1] == '1' && !visited[i][j+1]) {
+                visited[i][j+1] = true;
+                q.offer(new XY(i, j + 1));
+            }
+        }
+    }
     static List<List<Integer>> adjMat2List(int[][] matrix){
         List<List<Integer>> adj = new ArrayList<>();
         for (int i = 0; i < matrix.length; i++) {
@@ -77,6 +128,6 @@ class Graph {
         return adj;
     }
     public static void main(String[] args) {
-
+        HashMap<Integer, Integer> map = new HashMap<>();
     }
 }
---
+++ src/com/dynamicProgramming/Fibonacci.java
@@ -0,0 +1,77 @@
+package com.dynamicProgramming;
+
+import java.util.Arrays;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        int n = 5;
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        System.out.println(recursion(n));
+        System.out.println(memoization(n, dp));
+        System.out.println(tabulation(n));
+        System.out.println(optimized(n));
+    }
+
+    /**
+     * Space optimized solution
+     * @param n index of fibonacci number
+     * @return fibonacci number of given index
+     * @SpaceComplexity O(1) ✅
+     */
+    static int optimized(int n){
+        if(n<=1) return n;
+        int prev = 0;
+        int curr = 1;
+        for (int i = 2; i <= n; i++) {
+            int next = prev + curr;
+            prev = curr;
+            curr = next;
+        }
+        return curr;
+    }
+    /**
+     * Tabulation method - suitable only for n below 40-45
+     * We started from bottom i.e., 0, 1 and 2 and went up filling the array
+     * @param n index of fibonacci number
+     * @return fibonacci number of given index
+     * @TimeComplexity O(N)
+     * @SpaceComplexity O(N) : Array + O(N) : Auxiliary Space
+     */
+    static int tabulation(int n){
+        if(n<=1) return n;
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+    /**
+     * Memoization method - suitable only for n below 40-45
+     * We started from top i.e., n and went down filling the array
+     * @param n index of fibonacci number
+     * @return fibonacci number of given index
+     * @TimeComplexity O(N)
+     * @SpaceComplexity O(N) : Array + O(N) : Auxiliary Space
+     */
+    static int memoization(int n, int[] dp){
+        if(n<=1) return n;
+        if(dp[n]!=-1) return dp[n];
+        dp[n] = memoization(n-1, dp) + memoization(n-2, dp);
+        return dp[n];
+    }
+
+    /**
+     * Recursion method - lots of overhead
+     * @param n index of fibonacci number
+     * @return fibonacci number of given index
+     * @TimeComplexity O(2^N)
+     * @SpaceComplexity O(N): Auxiliary Space
+     */
+    static int recursion(int n){
+        if(n<=1) return n;
+        return recursion(n-1) + recursion(n-2);
+    }
+}
