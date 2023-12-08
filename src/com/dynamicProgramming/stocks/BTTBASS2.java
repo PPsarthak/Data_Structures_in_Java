790198b78bb6d0a584a0f3b04c9d07094969f84f
Psarthak <panditsarthak1553@gmail.com>
2023-12-08 18:19:21 +0530

    DP-35 & DP-36

--- src/com/dynamicProgramming/stocks/BTTBASS2.java
+++ src/com/dynamicProgramming/stocks/BTTBASS2.java
@@ -2,14 +2,40 @@ package com.dynamicProgramming.stocks;
 
 import java.util.Arrays;
 
+/**
+ * Based on DP-36
+ * Still not proper DP Que, can be solved using Greedy
+ * The leetcode method (fucking fastest) just finds the instances where we have a profit (no matter
+ * how big or small the profit is) and adds it to total
+ *
+ * REMEMBER (IF YOU ARE LOOKING AT GREEDY APPROACH),
+ *      WE CAN SELL AND THEN AGAIN BUY ON THE SAME DAY i.e., WE CAN PROLONG IT IN THIS WAY
+ *
+ * PS: THIS THINKING IS DONE AUTOMATICALLY BY DP APPROACH
+ */
 public class BTTBASS2 {
     public static void main(String[] args) {
-        int[] arr = {7,1,5,3,6,4}; // ans:= 1 and 5 + 3 and 6 => 4 + 3 = 7
+        int[] arr = {7,1,3,5,4,3,6}; // ans:= 1 and 5 + 3 and 6 => 4 + 3 = 7
 
         System.out.println(recursive(arr, 0, true));
 
         int[][] dp = new int[arr.length][2];
         for(int[] i : dp) Arrays.fill(i,-1);
+
+        System.out.println(memoize(arr, 0, 1, dp));
+
+        System.out.println(fuckingFast(arr));
+    }
+    static int fuckingFast(int[] arr){
+        //approach: buy and sell max no of times to get max profit
+        int profit = 0;
+        for (int i = 0; i < (arr.length-1); i++){
+            if (arr[i] < arr[i+1]){
+                System.out.println(arr[i+1] + " " + arr[i]);
+                profit += arr[i+1] - arr[i];
+            }
+        }
+        return profit;
     }
     static int recursive(int[] arr, int index, boolean canBuy){
         if(index == arr.length) return 0;
