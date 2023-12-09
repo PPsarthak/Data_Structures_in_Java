43d262bdc6a4437301c3654095bbd343703da20f
Psarthak <panditsarthak1553@gmail.com>
2023-12-09 10:59:12 +0530

    DP-38

--- src/com/dynamicProgramming/stocks/BTTBASS4.java
+++ src/com/dynamicProgramming/stocks/BTTBASS4.java
@@ -2,7 +2,7 @@ package com.dynamicProgramming.stocks;
 
 import java.util.Arrays;
 
-public class BTBASS4 {
+public class BTTBASS4 {
     public static void main(String[] args) {
         int[] arr = {3,3,5,0,0,3,1,4};
         int k = 2;
